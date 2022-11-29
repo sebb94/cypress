580254e3b338ac5ed7579a28b71d092f1becc07a
sebb94 <omgl0lwth@gmail.com>
2022-11-29 10:48:33 +0100

    ui challenges

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import "cypress-real-events";
 require('cypress-xpath');
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
